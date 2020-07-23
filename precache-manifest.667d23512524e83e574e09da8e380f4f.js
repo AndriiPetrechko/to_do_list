9373fa4bd244223c1932c3ec4e3265d58ac5c27a
AndriiPetrechko <petrechkoandrij@gmail.com>
2020-07-23 23:06:40 +0300

    Updates

---
+++ precache-manifest.667d23512524e83e574e09da8e380f4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef63168359bd627f5ccd591d12a7d93",
+    "url": "/to_do_list/index.html"
+  },
+  {
+    "revision": "12deea38386bd646ffe6",
+    "url": "/to_do_list/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c31ea6fb4b75db61daf1",
+    "url": "/to_do_list/static/css/main.1ffd7e98.chunk.css"
+  },
+  {
+    "revision": "12deea38386bd646ffe6",
+    "url": "/to_do_list/static/js/2.fa873329.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/to_do_list/static/js/2.fa873329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c31ea6fb4b75db61daf1",
+    "url": "/to_do_list/static/js/main.314b9491.chunk.js"
+  },
+  {
+    "revision": "211d90c18bf12326ba84",
+    "url": "/to_do_list/static/js/runtime-main.c6bad940.js"
+  }
+]);
